2d6b37df83dbb8a59aa98d471dbdc1ff1062ca57
Loan <loan.matheus@hotmail.com>
2021-12-27 12:17:44 -0300

    Atualizando Exercicios Javascprit

---
+++ Fundamentos do Desenvolvimento Web/Exercicios/Bloco 04 - Javascript/1 - Primeiros passos/Exercicios.js
@@ -0,0 +1,17 @@
+let estado = "lista";
+switch (estado) {
+  case "aprovado":
+    console.log("Parabéns, você foi aprovada(o)!");
+    break;
+
+  case "lista":
+    console.log("Você está na nossa lista de espera");
+    break;
+
+  case "reprovado":
+    console.log("Você foi reprovada(o)");
+    break;
+
+  default:
+    console.log("não listado")
+  }
