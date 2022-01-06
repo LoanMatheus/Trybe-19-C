908d936e22f940a67329df0418ee3e3fe3de9582
Loan <loan.matheus@hotmail.com>
2022-01-06 01:19:29 -0300

    Bloco 5.1

---
+++ Fundamentos do Desenvolvimento Web/Exercicios/Bloco 05 - JavaScript: DOM, Eventos e Web Storage/1 - JavaScript - DOM e seletores/script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll("#page-title")[0].innerText = 'Jogador Nº1';
+document.querySelectorAll("#second-paragraph")[0].innerText = 'Ainda irei ler o livro';
+document.querySelector("#subtitle").innerText = 'Melhores Easter-Eggs'
+
+let tagP = document.querySelectorAll('p');
+for(let i = 0; i < tagP.length; i += 1){
+  tagP[i].classList.add('paragrafo');
+}
+
+document.getElementsByTagName('h4').subtitle.style.color = 'blue'
+
+
